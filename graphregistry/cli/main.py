7909f0dd31ea183bff5d514d2961a656f0469eb2
fpereira-epfl <francisco.pinto@epfl.ch>
2026-01-21 08:49:40 +0100

    Added GraphAI test command.

--- graphregistry/cli/main.py
+++ graphregistry/cli/main.py
@@ -7,6 +7,12 @@ from graphregistry.clients.mysql         import GraphDB
 from graphregistry.clients.elasticsearch import GraphES
 # from graphregistry.core.registry         import GraphRegistry
 
+# Import GraphAI client
+import graphai_client as GraphAI
+import graphai_client.client_api
+import graphai_client.client
+
+
 def build_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         prog="graphregistry",
@@ -14,7 +20,7 @@ def build_parser() -> argparse.ArgumentParser:
     )
     subparsers = parser.add_subparsers(dest="domain", required=True)
 
-    for cmd_name in ['airflow', 'cache', 'db', 'index']:
+    for cmd_name in ['ai', 'airflow', 'cache', 'db', 'index']:
         register(subparsers, cmd_name)
 
     return parser
@@ -34,18 +40,32 @@ def main(argv=None) -> int:
     db       = GraphDB()
     index    = GraphES()
     # registry = GraphRegistry()
+    ai       = GraphAI
 
-    ctx = CLIContext(global_config=global_config, index_config=index_config, scores_config=scores_config, db=db, index=index) #, registry=registry)
+    # Login to GraphAI and get auth token
+    graphai_auth_token = ai.client.login(global_config.settings['graphai']['client_config_file'])
+
+    # Create CLI context
+    ctx = CLIContext(
+        global_config = global_config,
+        index_config  = index_config,
+        scores_config = scores_config,
+        db    = db,
+        index = index,
+        ai    = ai,
+        graphai_auth_token = graphai_auth_token
+    ) #, registry=registry)
     args.ctx = ctx   # attach to args for all subcommands
 
-    try:
-        return args.func(args) or 0
-    except KeyboardInterrupt:
-        print("\nInterrupted by user.", file=sys.stderr)
-        return 130
-    except Exception as exc:
-        print(f"❌ ERROR: {exc}", file=sys.stderr)
-        return 1
-    finally:
-        # Optionally close resources explicitly if your clients expose it
-        pass
+    return args.func(args) or 0
+    # try:
+    #     return args.func(args) or 0
+    # except KeyboardInterrupt:
+    #     print("\nInterrupted by user.", file=sys.stderr)
+    #     return 130
+    # except Exception as exc:
+    #     print(f"❌ ERROR: {exc}", file=sys.stderr)
+    #     return 1
+    # finally:
+    #     # Optionally close resources explicitly if your clients expose it
+    #     pass
