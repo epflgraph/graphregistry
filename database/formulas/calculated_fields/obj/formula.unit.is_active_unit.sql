3d9b02945e55026718647d9b7434ac89800f2ba6
fpereira-epfl <francisco.pinto@epfl.ch>
2026-01-29 10:32:39 +0100

    Simplified formulas.

--- database/formulas/calculated_fields/obj/formula.unit.is_active_unit.sql
+++ database/formulas/calculated_fields/obj/formula.unit.is_active_unit.sql
@@ -1,35 +1,30 @@
-    SELECT n.institution_id, n.object_type, n.object_id, 'n/a' AS field_language, 'is_active_unit' AS field_name,
-           
-           CASE
-           WHEN EXISTS (SELECT 1
-                          FROM (SELECT cf.object_id,
-                                  CASE
-                                  WHEN cf.field_value REGEXP '^[0-9]{4}-[0-9]{2}-[0-9]{2}$'
-                                  THEN STR_TO_DATE(cf.field_value, '%Y-%m-%d')
-                                  WHEN cf.field_value REGEXP '^[0-9]{4}-[0-9]{2}-[0-9]{2}[ T][0-9]{2}:[0-9]{2}:[0-9]{2}$'
-                                  THEN STR_TO_DATE(cf.field_value, '%Y-%m-%d %H:%i:%s')
-                                  ELSE NULL END AS dt
-                                  FROM [[registry]].Data_N_Object_T_CustomFields cf
-                                 WHERE cf.object_type    = 'Unit'
-                                   AND cf.field_language = 'n/a'
-                                   AND cf.field_name     = 'date_terminated'
-                                   AND cf.field_value IS NOT NULL) AS dt_cf
-                         WHERE dt_cf.object_id = n.object_id
-                           AND dt_cf.dt IS NOT NULL
-                           AND dt_cf.dt < NOW()) THEN 0 ELSE 1 END AS field_value
+    SELECT n.institution_id, n.object_type, n.object_id,
+           'n/a' AS field_language,
+           'is_active_unit' AS field_name,
+      
+        -- Conditions for the Unit being active: no end date or end date in the future
+           (e2.field_value IS NULL OR CAST(e2.field_value AS DATETIME) > NOW()) AS field_value
+	  
+        -- Start with all Unit objects
+      FROM graph_registry.Nodes_N_Object n
 
-      FROM [[registry]].Nodes_N_Object n
-
+        -- Append 'established' and 'terminated' dates
+ LEFT JOIN graph_registry.Data_N_Object_T_CustomFields e1
+        ON (n.institution_id, n.object_type, n.object_id) = (e1.institution_id, e1.object_type, e1.object_id)
+       AND e1.field_name = 'date_established'
+ LEFT JOIN graph_registry.Data_N_Object_T_CustomFields e2
+        ON (n.institution_id, n.object_type, n.object_id) = (e2.institution_id, e2.object_type, e2.object_id)
+       AND e2.field_name = 'date_terminated'
+        
         -- Check object flags
-INNER JOIN [[airflow]].Operations_N_Object_T_FieldsChanged tp
-     USING (institution_id, object_type, object_id)
+INNER JOIN graph_airflow.Operations_N_Object_T_FieldsChanged tp
+        ON (n.institution_id, n.object_type, n.object_id) = (tp.institution_id, tp.object_type, tp.object_id)
 
         -- Check type flags
-INNER JOIN [[airflow]].Operations_N_Object_T_TypeFlags tf
-     USING (institution_id, object_type)
+INNER JOIN graph_airflow.Operations_N_Object_T_TypeFlags tf
+        ON (n.institution_id, n.object_type) = (tf.institution_id, tf.object_type)
 
      WHERE n.object_type = 'Unit'
-
        AND tp.to_process = 1
        AND tf.to_process = 1
        AND tf.flag_type = 'fields'
