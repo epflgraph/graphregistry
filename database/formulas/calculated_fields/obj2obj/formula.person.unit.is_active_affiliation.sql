505acde6acf6c8524c8d69c99dd60a09185ecf4e
fpereira-epfl <francisco.pinto@epfl.ch>
2026-01-29 10:55:16 +0100

    Corrected the no longer supported EXISTS command and simplified.

--- database/formulas/calculated_fields/obj2obj/formula.person.unit.is_active_affiliation.sql
+++ database/formulas/calculated_fields/obj2obj/formula.person.unit.is_active_affiliation.sql
@@ -1,15 +1,17 @@
-    SELECT n.from_institution_id, n.from_object_type, n.from_object_id,
-           n.to_institution_id,   n.to_object_type,   n.to_object_id,
-           'n/a' AS field_language, 'is_active_affiliation' AS field_name,
-           CASE
-           WHEN EXISTS (SELECT 1
-                          FROM [[registry]].Data_N_Object_N_Object_T_CustomFields cf
-                         WHERE (cf.from_object_id, cf.from_object_type, cf.to_object_id, cf.to_object_type, cf.field_language, cf.field_name)
-                             = (n.from_object_id, 'Person', n.to_object_id, 'Unit', 'n/a', 'end_datetime')
-                           AND cf.field_value IS NOT NULL
-                           AND CAST(cf.field_value AS DATETIME) < NOW()
-           ) THEN 0 ELSE 1 END AS field_value
-      FROM [[registry]].Data_N_Object_N_Object_T_CustomFields n
+    SELECT e.from_institution_id   AS from_institution_id,
+           e.from_object_type      AS from_object_type,
+           e.from_object_id        AS from_object_id,
+           e.to_institution_id     AS to_institution_id,
+           e.to_object_type        AS to_object_type,
+           e.to_object_id          AS to_object_id,
+           'n/a'                   AS field_language,
+           'is_active_affiliation' AS field_name,
+ 
+        -- Conditions for being an active Person-Unit affiliation: no end date or end date in the future
+           (e.field_value IS NULL OR CAST(e.field_value AS DATETIME) > NOW()) AS field_value
+
+        -- Start with people-unit affiliations
+      FROM [[registry]].Data_N_Object_N_Object_T_CustomFields e
 
         -- Check object flags
 INNER JOIN [[airflow]].Operations_N_Object_N_Object_T_FieldsChanged tp
@@ -19,7 +21,7 @@ INNER JOIN [[airflow]].Operations_N_Object_N_Object_T_FieldsChanged tp
 INNER JOIN [[airflow]].Operations_N_Object_N_Object_T_TypeFlags tf
      USING (from_institution_id, from_object_type, to_institution_id, to_object_type)
 
-     WHERE (n.from_object_type, n.to_object_type) = ('Person', 'Unit')
-
+     WHERE (e.from_object_type, e.to_object_type) = ('Person', 'Unit')
+       AND e.field_name = 'end_datetime'
        AND tp.to_process = 1
        AND tf.to_process = 1
