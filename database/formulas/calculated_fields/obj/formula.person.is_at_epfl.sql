30028a468b37479e81ac1f9f25997a508f33e069
fpereira-epfl <francisco.pinto@epfl.ch>
2026-01-29 10:32:39 +0100

    Simplified formulas.

--- database/formulas/calculated_fields/obj/formula.person.is_at_epfl.sql
+++ database/formulas/calculated_fields/obj/formula.person.is_at_epfl.sql
@@ -1,14 +1,16 @@
-    SELECT e.from_institution_id AS institution_id, e.from_object_type AS object_type, e.from_object_id AS object_id, 'n/a' AS field_language, 'is_at_epfl' AS field_name,
-           CASE
-           WHEN EXISTS (SELECT 1
-                          FROM [[registry]].Data_N_Object_N_Object_T_CustomFields cf
-                         WHERE (cf.from_institution_id, cf.from_object_type, cf.from_object_id, cf.to_institution_id, cf.to_object_type, cf.to_object_id, cf.context)
-                             = ( e.from_institution_id,  e.from_object_type,  e.from_object_id,  e.to_institution_id,  e.to_object_type,  e.to_object_id,  e.context)
-                           AND (cf.field_language, cf.field_name) = ('n/a', 'end_datetime')
-                           AND cf.field_value IS NOT NULL
-                           AND CAST(cf.field_value AS DATETIME) < NOW()
-           ) THEN 0 ELSE 1 END AS field_value
-      FROM [[registry]].Edges_N_Object_N_Object_T_ChildToParent e
+    SELECT e.from_institution_id AS institution_id,
+           e.from_object_type    AS object_type,
+           e.from_object_id      AS object_id,
+           'n/a'                 AS field_language,
+           'is_at_epfl'          AS field_name,
+ 
+        -- Conditions for being at EPFL: no end date or end date in the future
+        -- NOTE: the max() is needed to aggregate multiple affiliations and keep a zero value if
+        -- all affiliations are expired.
+           MAX( (e.field_value IS NULL OR CAST(e.field_value AS DATETIME) > NOW()) ) AS field_value
+
+        -- Start with people-unit affiliations
+      FROM [[registry]].Data_N_Object_N_Object_T_CustomFields e
 
         -- Check object flags
 INNER JOIN [[airflow]].Operations_N_Object_N_Object_T_FieldsChanged tp
@@ -19,6 +21,8 @@ INNER JOIN [[airflow]].Operations_N_Object_N_Object_T_TypeFlags tf
      USING (from_institution_id, from_object_type, to_institution_id, to_object_type)
 
      WHERE (e.from_object_type, e.to_object_type) = ('Person', 'Unit')
-
+       AND e.field_name = 'end_datetime'
        AND tp.to_process = 1
        AND tf.to_process = 1
+
+  GROUP BY e.from_object_id
