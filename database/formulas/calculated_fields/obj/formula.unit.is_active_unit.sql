c1aa0d3000c836227fd17bb58230f05b2dc72fa7
fpereira-epfl <francisco.pinto@epfl.ch>
2026-03-09 15:59:07 +0100

    Fixed bug with schema placeholders.

--- database/formulas/calculated_fields/obj/formula.unit.is_active_unit.sql
+++ database/formulas/calculated_fields/obj/formula.unit.is_active_unit.sql
@@ -1,27 +1,27 @@
     SELECT n.institution_id, n.object_type, n.object_id,
            'n/a' AS field_language,
            'is_active_unit' AS field_name,
-      
+
         -- Conditions for the Unit being active: no end date or end date in the future
            (e2.field_value IS NULL OR CAST(e2.field_value AS DATETIME) > NOW()) AS field_value
-	  
+
         -- Start with all Unit objects
-      FROM graph_registry.Nodes_N_Object n
+      FROM [[registry]].Nodes_N_Object n
 
         -- Append 'established' and 'terminated' dates
- LEFT JOIN graph_registry.Data_N_Object_T_CustomFields e1
+ LEFT JOIN [[registry]].Data_N_Object_T_CustomFields e1
         ON (n.institution_id, n.object_type, n.object_id) = (e1.institution_id, e1.object_type, e1.object_id)
        AND e1.field_name = 'date_established'
- LEFT JOIN graph_registry.Data_N_Object_T_CustomFields e2
+ LEFT JOIN [[registry]].Data_N_Object_T_CustomFields e2
         ON (n.institution_id, n.object_type, n.object_id) = (e2.institution_id, e2.object_type, e2.object_id)
        AND e2.field_name = 'date_terminated'
-        
+
         -- Check object flags
-INNER JOIN graph_airflow.Operations_N_Object_T_FieldsChanged tp
+INNER JOIN [[airflow]].Operations_N_Object_T_FieldsChanged tp
         ON (n.institution_id, n.object_type, n.object_id) = (tp.institution_id, tp.object_type, tp.object_id)
 
         -- Check type flags
-INNER JOIN graph_airflow.Operations_N_Object_T_TypeFlags tf
+INNER JOIN [[airflow]].Operations_N_Object_T_TypeFlags tf
         ON (n.institution_id, n.object_type) = (tf.institution_id, tf.object_type)
 
      WHERE n.object_type = 'Unit'
