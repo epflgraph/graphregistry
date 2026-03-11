fa6f2ca9ec46d231d4d9203ef186b15f0fb33e8c
fpereira-epfl <francisco.pinto@epfl.ch>
2026-03-11 20:19:20 +0100

    Missing formulas.

---
+++ database/formulas/calculated_fields/obj/formula.course.latest_academic_year.sql
@@ -0,0 +1,24 @@
+    SELECT e.from_institution_id  AS institution_id,
+           e.from_object_type     AS object_type,
+           e.from_object_id       AS object_id,
+           'n/a'                  AS field_language,
+           'latest_academic_year' AS field_name,
+           MAX(e.field_value)     AS field_value
+
+        -- Start with people-unit affiliations
+      FROM [[registry]].Data_N_Object_N_Object_T_CustomFields e
+
+        -- Check object flags
+INNER JOIN [[airflow]].Operations_N_Object_N_Object_T_FieldsChanged tp
+     USING (from_institution_id, from_object_type, from_object_id, to_institution_id, to_object_type, to_object_id)
+
+        -- Check type flags
+INNER JOIN [[airflow]].Operations_N_Object_N_Object_T_TypeFlags tf
+     USING (from_institution_id, from_object_type, to_institution_id, to_object_type)
+
+     WHERE (e.from_object_type, e.to_object_type, e.context) = ('Course', 'Person', 'teacher')
+       AND e.field_name = 'teaching_assignment_year'
+       AND tp.to_process = 1
+       AND tf.to_process = 1
+
+   GROUP BY e.from_object_id
