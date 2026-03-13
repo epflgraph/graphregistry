851c2573589279dbab16da2cafcb96e15a9b16e1
fpereira-epfl <francisco.pinto@epfl.ch>
2026-03-13 09:05:32 +0100

    Debug update.

--- graphregistry/cli/cmd_cache.py
+++ graphregistry/cli/cmd_cache.py
@@ -132,9 +132,18 @@ def cmd_cache_debug(args):
     #     output_file='./output.csv'
     # )
 
-    print('Atempt 2RVGrr2')
-    registry.indexes.generate_local_cache_streaming( index_date='2026-03-03', ignore_warnings=False, replace_existing=True, force_replace=True)
-    registry.indexes.generate_index_from_local_cache(index_date='2026-03-03', ignore_warnings=False, replace_existing=True, force_replace=True)
+    # print('Atempt 2RVGrr2')
+    # registry.indexes.generate_local_cache_streaming( index_date='2026-03-03', ignore_warnings=False, replace_existing=True, force_replace=True)
+    # registry.indexes.generate_index_from_local_cache(index_date='2026-03-03', ignore_warnings=False, replace_existing=True, force_replace=True)
+
+
+    from graphregistry.common.config import GlobalConfig
+    glbcfg = GlobalConfig()
+    db.print_database_stats(engine_name='xaas_coresrv', schema_name=glbcfg.settings['mysql']['db_schema_names']['graphsearch_test']   , re_exclude=[r'.*(MOOC|Lecture|Widget|Notebook|Exercise|Specialisation|Startup|StudyPlan).*'])
+    # db.print_database_stats(engine_name='xaas_coresrv', schema_name=glbcfg.settings['mysql']['db_schema_names']['elasticsearch_cache'], re_exclude=[r'.*(MOOC|Lecture|Widget|Notebook|Exercise).*'])
+
+
+    # registry.indexdb.idoclinks['Category']['Course']['SEM'].horizontal_patch(actions=('print', 'commit'))
 
     # Print footers
     print("🖥️  ~ Done.")
