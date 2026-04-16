21a94d34e310c8a5bd47bb44cebd07c69abc8758
fpereira-epfl <francisco.pinto@epfl.ch>
2026-04-17 00:07:17 +0200

    🔧 Change actions from 'eval' to 'commit' for node and edge list insertions in sample_course_test.sh

--- sample_course_test.sh
+++ sample_course_test.sh
@@ -12,5 +12,5 @@ graphregistry data fetch --edge=EPFL,Course,TEST-101,EPFL,Person,01010101,teache
 graphregistry data insert --node=@sample_course_node.json --actions=commit
 graphregistry data delete --node=EPFL,Course,TEST-101 --actions=commit
 
-graphregistry data insert --node_list=@scripts/init/sample_sets/epfl_graph_sample_set_NODEs.json --actions=eval
-graphregistry data insert --edge_list=@scripts/init/sample_sets/epfl_graph_sample_set_EDGEs.json --actions=eval
+graphregistry data insert --node_list=@scripts/init/sample_sets/epfl_graph_sample_set_NODEs.json --actions=commit
+graphregistry data insert --edge_list=@scripts/init/sample_sets/epfl_graph_sample_set_EDGEs.json --actions=commit
