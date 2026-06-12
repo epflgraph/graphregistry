a9653db15b0889e8d98306f951dfc54ce8f59a77
fpereira-epfl <francisco.pinto@epfl.ch>
2026-06-12 18:16:45 +0200

    🚧 testing

--- scripts/t_lecture_process.py
+++ scripts/t_lecture_process.py
@@ -120,18 +120,18 @@ if False:
     rich.print(f"Audio token: {out_token}")
     rich.print(f"Audio token matches: {out_token == result['token']}")
     
-if True:
+if False:
     
     # Get a list of lectures with undetected slides and print the first lecture key
-    # list_of_lectures = lecture_ops.get_with_undetected_slides(limit=16)
-    # for f in list_of_lectures.item_list:
-    #     rich.print(f)
-       
+    list_of_lectures = lecture_ops.get_with_undetected_slides(limit=16)
+    for f in list_of_lectures.item_list:
+        rich.print(f)
+    exit()
     # For demonstration purposes, we will use the test key instead of the retrieved lecture keys 
     lecture_key = NodeKey(
         institution_id = 'EPFL',
         object_type    = 'Lecture',
-        object_id      = '0_005blefe'
+        object_id      = '0_042zj2ns'
     )
     
     # Get video token
@@ -139,8 +139,8 @@ if True:
     rich.print(f"Video token: {video_token}")
     
     # Launch slide detection
-    # task_id = lecture_ops.launch_slide_detection(video_token)
-    task_id = 'fe5a497d-8ff9-4884-af51-09e79a4e51dc'
+    # task_id = lecture_ops.launch_slide_detection(video_token, no_cache=True)
+    task_id = '2ca432a3-8eba-4212-b334-9dbb076f9573'
     rich.print(f"Launched slide detection with task ID: {task_id}")
     
     # Save the slide detection task ID in the database
@@ -149,8 +149,48 @@ if True:
     # Retrieve the slide detection task ID from the database and print it
     rich.print(f"Slide detection task ID: {lecture_ops.get_slide_detection_task_id(lecture_key)}")
     
+    unfinished_tasks = lecture_ops.get_unfinished_slide_detection_tasks()
+    rich.print(f"Unfinished slide detection tasks: {unfinished_tasks}")
+    
     # Get the slide detection result using the task ID and print it
     result = lecture_ops.get_slide_detection_result(lecture_key)
     rich.print(f"Slide detection result:")
     rich.print_json(data=result)
     
+    # Check if the result is not None before proceeding
+    assert result is not None, "Slide detection result is None"
+    
+    # Get slide number and tokens from the result
+    slide_num_and_tokens = [(int(k), v["token"]) for k,v in result["task_result"]["slide_tokens"].items()]
+
+    # Save slide tokens in the database
+    lecture_ops.save_slide_tokens(lecture_key=test_key, slide_num_and_tokens=slide_num_and_tokens)
+
+
+
+if True:
+    
+    if True:
+    
+        list_of_lectures = lecture_ops.get_with_undetected_slides(limit=16)
+        for lecture_key in list_of_lectures.item_list:
+            rich.print(lecture_key)
+            video_token = lecture_ops.get_video_token(lecture_key)
+            rich.print(f"Video token: {video_token}")
+            task_id = lecture_ops.launch_slide_detection(video_token, no_cache=True)
+            rich.print(f"Launched slide detection with task ID: {task_id}")
+            assert task_id is not None, "Failed to launch slide detection"
+            lecture_ops.save_slide_detection_task_id(lecture_key, task_id)
+            rich.print(f"Slide detection task ID: {lecture_ops.get_slide_detection_task_id(lecture_key)}")
+            
+    else:
+        
+        unfinished_tasks = lecture_ipops.get_unfinished_slide_detection_tasks()
+        for lecture_key in unfinished_tasks.item_list:
+            print(f"Checking slide detection result for lecture with key {lecture_key}...")
+            result = lecture_ops.get_slide_detection_result(lecture_key)
+            rich.print(f"Slide detection result:")
+            rich.print_json(data=result)
+            
+            
+            
