fd5bcce3dc9947db104f7cb05f49f005a5646374
fpereira-epfl <francisco.pinto@epfl.ch>
2026-07-06 13:14:12 +0200

    🔧 Enhance SQL query in node_get_with_no_concepts to include processing flags for fields and type

--- database/queries/registry/commit/node_get_with_no_concepts.sql
+++ database/queries/registry/commit/node_get_with_no_concepts.sql
@@ -3,7 +3,13 @@ SELECT DISTINCT institution_id, object_type, object_id
            FROM [[registry]].Nodes_N_Object n
       LEFT JOIN [[registry]].Edges_N_Object_N_Concept_T_ConceptDetection c
           USING (institution_id, object_type, object_id)
+     INNER JOIN [[airflow]].Operations_N_Object_T_FieldsChanged fc
+          USING (institution_id, object_type, object_id)
+     INNER JOIN [[airflow]].Operations_N_Object_T_TypeFlags tf
+          USING (institution_id, object_type)
           WHERE n.object_type LIKE '[[object_type]]'
-            AND n.object_id LIKE '[[id_pattern]]'
-            AND n.raw_text IS NOT NULL
-            AND c.concept_id IS NULL;
+            AND n.object_id   LIKE '[[id_pattern]]'
+            AND n.raw_text    IS NOT NULL
+            AND c.concept_id  IS NULL
+            AND fc.to_process = 1
+            AND tf.to_process = 1
