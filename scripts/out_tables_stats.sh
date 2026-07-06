426f901fc2c6b1f207c13808f7ba8381a21e04a8
fpereira-epfl <francisco.pinto@epfl.ch>
2026-07-06 13:16:22 +0200

    🔧 Comment out development schema name to prevent confusion; use production schema name for database stats.

--- scripts/out_tables_stats.sh
+++ scripts/out_tables_stats.sh
@@ -7,7 +7,8 @@ glbcfg = GlobalConfig()
 
 db_env = "xaas_coresrv"
 
-schema_name = '_1_DEV_' + glbcfg.settings['mysql']['db_schema_names']['graphsearch_test']
+# schema_name = '_1_DEV_' + glbcfg.settings['mysql']['db_schema_names']['graphsearch_test']
+schema_name = glbcfg.settings['mysql']['db_schema_names']['graphsearch_test']
 
 db.print_database_stats(
     engine_name=db_env,
