74ea76ca7e4a26dfdfa3b019b4a31dc81d0eaf67
fpereira-epfl <francisco.pinto@epfl.ch>
2026-07-06 13:17:43 +0200

    🗑️ no longer needed

--- database/queries/search/commit/no_loose_ends_refresh.sql
+++
@@ -1,4 +0,0 @@
-TRUNCATE TABLE graph_cache.Operations_N_Object_T_NoLooseEnds;
-   INSERT INTO graph_cache.Operations_N_Object_T_NoLooseEnds (object_type, object_id) SELECT object_type, object_id FROM graph_registry.Data_N_Object_T_PageProfile;
-   INSERT INTO graph_cache.Operations_N_Object_T_NoLooseEnds (object_type, object_id) SELECT object_type, object_id FROM graph_lectures.Data_N_Object_T_PageProfile;
-   INSERT INTO graph_cache.Operations_N_Object_T_NoLooseEnds (object_type, object_id) SELECT object_type, object_id FROM graph_ontology.Data_N_Object_T_PageProfile;
