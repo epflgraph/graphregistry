7a22989775ad29f150e436ceb35901c2f796dec4
fpereira-epfl <francisco.pinto@epfl.ch>
2026-07-30 15:23:22 +0200

    🔧 Add SQL scripts for deleting rows in doclinks and page profiles

---
+++ database/queries/patching/commit/index_doclinks_delete_rows.sql
@@ -0,0 +1,6 @@
+       -- Execute DELETE rows query
+   DELETE p
+     FROM [[graphsearch_prod_mirror]].Index_D_[[doc_type]]_L_[[link_type]]_T_[[sem_or_org]][[special_suffix]] p
+LEFT JOIN        [[graphsearch_test]].Index_D_[[doc_type]]_L_[[link_type]]_T_[[sem_or_org]][[special_suffix]] t
+    USING (doc_type, doc_id, link_type, link_subtype, link_id)
+    WHERE t.doc_id IS NULL;
---
+++ database/queries/patching/commit/index_docs_delete_rows.sql
@@ -0,0 +1,6 @@
+       -- Execute DELETE rows query
+   DELETE p
+     FROM [[graphsearch_prod_mirror]].Index_D_[[doc_type]] p
+LEFT JOIN        [[graphsearch_test]].Index_D_[[doc_type]] t
+    USING (doc_type, doc_id)
+    WHERE t.doc_id IS NULL;
