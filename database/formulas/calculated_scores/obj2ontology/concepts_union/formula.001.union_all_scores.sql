acb86966ac108e0cacd7c77176bb1330ca1d4bcf
fpereira-epfl <francisco.pinto@epfl.ch>
2026-08-18 18:43:50 +0200

    🔧 added "delete" condition plus some simplifications

--- database/formulas/calculated_scores/obj2ontology/concepts_union/formula.001.union_all_scores.sql
+++ database/formulas/calculated_scores/obj2ontology/concepts_union/formula.001.union_all_scores.sql
@@ -4,14 +4,9 @@
                  SELECT object_type, object_id, concept_id, calculation_type, score, to_process
                    FROM (SELECT s.object_type, s.object_id, s.concept_id, s.calculation_type, s.score, 1 AS to_process
                            FROM [[graph_cache]].Edges_N_Object_N_Concept_T_CalculatedScores s
-                     INNER JOIN [[airflow]].Operations_N_Object_T_ScoresExpired se
-                          USING (object_type, object_id)
-                     INNER JOIN [[airflow]].Operations_N_Object_T_TypeFlags tf
-                          USING (object_type)
-                          WHERE se.to_process = 1
-                            AND tf.flag_type  = 'scores'
-                            AND tf.to_process = 1
-                             AND (    (s.object_type, s.calculation_type) = ('Course'  , 'average coverage over all lectures (bounded)')
+                          WHERE s.deleted = 0
+                            AND s.to_process = 1
+                            AND (     (s.object_type, s.calculation_type) = ('Course'  , 'average coverage over all lectures (bounded)')
                                    OR (s.object_type, s.calculation_type) = ('Lecture' ,     'percentage coverage in lecture (bounded)')
                                    OR (s.object_type, s.calculation_type) = ('Lecture' ,             'LLM keyword extraction (bounded)')
                                    OR (s.object_type, s.calculation_type) = ('Lecture' ,       'slide sum-scores aggregation (bounded)')
@@ -20,7 +15,7 @@
                                    OR (s.object_type, s.calculation_type) = ('Person'  ,    'abstract sum-scores aggregation (bounded)')
                                    OR (s.object_type, s.calculation_type) = ('Unit'    ,    'abstract sum-scores aggregation (bounded)')
                                    OR (s.object_type, s.calculation_type) = ('Category',     'concept sum-scores aggregation (bounded)')
-                                 )
+                                )
                             AND s.score >= 0.1
                         ) AS new
 ON DUPLICATE KEY UPDATE score      = new.score,
@@ -31,16 +26,16 @@ ON DUPLICATE KEY UPDATE score      = new.score,
                        (object_type, object_id, concept_id, calculation_type, score, to_process)
                  SELECT object_type, object_id, concept_id, calculation_type, score, to_process
                    FROM (SELECT s.object_type, s.object_id, s.concept_id, CONCAT('concept detection on ', s.text_source) AS calculation_type, s.score, 1 AS to_process
-                            FROM [[registry]].Edges_N_Object_N_Concept_T_ConceptDetection s
-                      INNER JOIN [[airflow]].Operations_N_Object_T_ScoresExpired se
-                           USING (object_type, object_id)
-                      INNER JOIN [[airflow]].Operations_N_Object_T_TypeFlags tf
-                           USING (object_type)
-                           WHERE s.record_deleted = 0
-                             AND se.to_process = 1
-                             AND tf.flag_type  = 'scores'
-                             AND tf.to_process = 1
-                             AND s.score >= 0.1
+                           FROM [[registry]].Edges_N_Object_N_Concept_T_ConceptDetection s
+                     INNER JOIN [[airflow]].Operations_N_Object_T_ScoresExpired se
+                          USING (object_type, object_id)
+                     INNER JOIN [[airflow]].Operations_N_Object_T_TypeFlags tf
+                          USING (object_type)
+                          WHERE s.record_deleted = 0
+                            AND se.to_process = 1
+                            AND tf.flag_type  = 'scores'
+                            AND tf.to_process = 1
+                            AND s.score >= 0.1
                         ) AS new
 ON DUPLICATE KEY UPDATE score      = new.score,
                         to_process = new.to_process;
